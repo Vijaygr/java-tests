cdbe5c1eda5f8c1ff3f88697bd95fec89d4e1ff2
Nicolas Huray <nicolas.huray@gmail.com.fr>
2013-03-18 16:55:10 -0400

    Implement StwAndo

---
+++ src/main/java/com/github/nhuray/javatests/triton/StwAndo.java
@@ -0,0 +1,23 @@
+package com.github.nhuray.javatests.triton;
+
+import com.google.common.base.Joiner;
+
+import static java.lang.Integer.toString;
+
+/**
+ * StwAndo
+ * Write a program that prints the numbers from 1 to 100. But for multiples of three print “Stw”
+ * instead of the number and for the multiples of five print “Ando”.
+ * For numbers which are multiples of both three and five print “StwAndo”.
+ */
+public class StwAndo {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            String replace = "";
+            if (i % 3 == 0) replace = "Stw";
+            if (i % 5 == 0) replace += "Ando";
+            System.out.println(replace.length() == 0 ? i : replace);
+        }
+    }
+}

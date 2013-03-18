f5945dd9d5c9f27332491fd2b82071e7c7a74520
Nicolas Huray <nicolas.huray@gmail.com.fr>
2013-03-18 17:57:20 -0400

    Change documentation

--- src/main/java/com/github/nhuray/javatests/triton/NumberTranslator.java
+++ src/main/java/com/github/nhuray/javatests/triton/NumberTranslator.java
@@ -1,7 +1,11 @@
 package com.github.nhuray.javatests.triton;
 
 /**
- * An utility class to translate numbers in plain english
+ * Algorithm
+ * <p>
+ * Given a number between 0 and 999,999, write a method that writes that number in the english language.
+ * For example a call with the number 100282 would return "One hundred thousand, two hundred eighty two"
+ * </p>
  */
 public class NumberTranslator {
 

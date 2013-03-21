0396ba055724c35ad4ab93d1c2ceae3d3410be3a
Nicolas Huray <nicolas.huray@gmail.com.fr>
2013-03-20 22:48:45 -0400

    Add a simple test requested by ypg

--- src/main/java/com/github/nhuray/javatests/ypg/Palindrome.java
+++ src/main/java/com/github/nhuray/javatests/ypg/Palindrome.java
@@ -6,7 +6,15 @@ package com.github.nhuray.javatests.ypg;
 public class Palindrome {
 
     public static boolean isPalindrome(String word) {
-        String reversed = new StringBuffer(word).reverse().toString();
-        return word.equals(reversed);
+        int length = word.length();
+        for (int i = 0; i < length / 2; i++) {
+            char a = word.charAt(i);
+            char b = word.charAt(length - 1 - i);
+            if (a != b) return false;
+        }
+        return true;
+        // Another way to do that is to use a StringBuffer
+        // String reversed = new StringBuffer(word).reverse().toString();
+        // return word.equals(reversed);
     }
 }
